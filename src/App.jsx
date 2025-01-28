0b4cb53de21c2439ba4c93d43967e23eea1f3faa
rafiKhan404 <rafidev404@gmail.com>
2025-01-28 11:19:28 +0600

    daisy ui setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Hello v4</h1>
+      <button className="btn btn-primary">Button</button>
+      <div className="rating">
+        <input
+          type="radio"
+          name="rating-2"
+          className="mask mask-star-2 bg-orange-400"
+        />
+        <input
+          type="radio"
+          name="rating-2"
+          className="mask mask-star-2 bg-orange-400"
+          defaultChecked
+        />
+        <input
+          type="radio"
+          name="rating-2"
+          className="mask mask-star-2 bg-orange-400"
+        />
+        <input
+          type="radio"
+          name="rating-2"
+          className="mask mask-star-2 bg-orange-400"
+        />
+        <input
+          type="radio"
+          name="rating-2"
+          className="mask mask-star-2 bg-orange-400"
+        />
       </div>
-      <h1>Assignment 8</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
